df1e4d15a99aa7147ad89b525ffe401e03ccf6d8
AbenaGold <priscy.o.arthur@gmail.com>
2024-08-15 15:11:58 +0000

    recent push

--- index.js
+++ index.js
@@ -4,15 +4,23 @@ import { dbconnection } from "./config/db.js";
 import MongoStore from "connect-mongo";
 import userRouter from "./routes/user.js";
 import itemRouter from "./routes/lost.js";
+import cors from "cors";
+import expressOasGenerator from "express-oas-generator"
 
 
 // Create an app
 const app = express();
+expressOasGenerator.handleRequests(app,{
+    alwaysServeDocs: true,
+    tags: ["User", "lost"],
+    mongooseModules: mongoose.moduleNames(),
+})
 
 dbconnection();
 
 // use middlewares
 app.use(express.json());
+app.use(cors({credentials:true, origin: '*'}));
 
 app.use(session({
     secret: process.env.SESSION_SECRET,
@@ -27,6 +35,8 @@ app.use(session({
 // use routes
 app.use(userRouter);
 app.use(itemRouter);
+expressOasGenerator.handleRequests();
+app.use((req, res) => res.redirect('/api-docs/'))
 
 
 // Listen for incoming request
